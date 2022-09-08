f3cd10f025a67653856864a672b7bfece4447f96
walid-ahmad <86514476+walid-ahmad@users.noreply.github.com>
2022-09-08 17:54:50 +0500

    checkout page completed

--- src/pages/checkout/checkout.page.tsx
+++ src/pages/checkout/checkout.page.tsx
@@ -1,4 +1,3 @@
-
 import React, { FC, Fragment, useRef, useState } from "react";
 import Link from "next/link";
 import { SETTINGS } from "../../constants/pageLinks";
@@ -7,104 +6,158 @@ import Image from "next/image";
 import Footer from "../../components/Footer";
 import Header from "../../components/Header";
 
-
 const CheckOut: FC = () => {
 	return (
-
 		<div className="">
 			{/* Header */}
 			<Header />
 			{/* Header End */}
 			<div className={`${css.my_container} mb-24`}>
-
 				<div className="flex flex-col items-center pt-[80px] pb-[60px] gap-9">
-
 					<h1 className="font-mulish font-bold text-[rgb(55,65,58,0.5)]">
 						2/2
 					</h1>
 					<div>
-
+						<h2 className={css.heading_styles}>Nearly there!</h2>
 						<h2 className={css.heading_styles}>
-							Nearly there!
-						</h2>
-						<h2 className={css.heading_styles}>
-
 							Just need a few details from you…
-
 						</h2>
 					</div>
-
 
 					<h3>🤘</h3>
-
 				</div>
-				<div className="px-3 py-8 md:px-[70px] md:py-[100px]  bg-[#FAFCF8]">
 
-					<div className="flex flex-col  items-center gap-20">
-						<h1 className={css.form_heading}>YOUR DETAIL</h1>
-
-						<div className="flex flex-col w-full gap-[30px]">
-							<div className={css.input_wrapper}>
-								{/* <label htmlFor="" className="">Name*</label> */}
-								<input type="text" name="" id="" placeholder="Name *" className={css.input_style} />
-							</div>
-							<div className={css.input_wrapper}>
-								{/* <label htmlFor="" className="">Name*</label> */}
-								<input type="text" name="" id="" placeholder="Your Email *" className={css.input_style} />
-							</div>
-							<div className={css.input_wrapper}>
-								{/* <label htmlFor="" className="">Name*</label> */}
-								<input type="text" name="" id="" placeholder="Phone Number *" className={css.input_style} />
-							</div>
-
-						</div>
-						<div className="flex flex-col items-center text-center md:items-center font-quicksand text-theme_dark_blue text-[14px] md:text-[16px]">
-							<p>Your email is only used for order confirmation/notifications and your phone</p>
-							<p>number only for delivery queries.</p>
-						</div>
-
-						<h1 className={css.form_heading}>DISCOUNT VOUCHER</h1>
-
-						
-						<div className={css.input_wrapper}>
-								{/* <label htmlFor="" className="">Name*</label> */}
-								<input type="text" name="" id="" placeholder="Enter Code" className={css.input_style} />
-							
-						</div>
-						<h1 className={css.form_heading}>PAYMENT</h1>
-
-						<div className="flex items-center w-full gap-4 border-b">
-							
-							
-										<div className="relative h-[15px] w-[20px] ">
-											<Image alt="card" src={'/assets/png/card.png'}  layout='fill' objectFit="contain" />
-										</div>
-										{/* <label htmlFor="" className="">Name*</label> */}
-										<input type="text" name="" id="" placeholder="Enter Code" className={`${css.input_style_card} flex-1`} />
-									
-							
+				<div className="flex flex-col gap-6">
+					<div className="px-4 sm:px-8 py-8 md:px-[150px] md:py-[100px] text-center bg-[#FAFCF8]">
+						<div className="flex flex-col  items-center gap-20">
+							<h1 className={css.form_heading}>YOUR DETAIL</h1>
+							<div className="flex flex-col w-full gap-[30px]">
+								<div className={css.input_wrapper}>
 									{/* <label htmlFor="" className="">Name*</label> */}
-									<input type="text" name="" id="" placeholder="MM / YY  CVC" className={css.input_style_card} />
-							
-					
+									<input
+										type="text"
+										name=""
+										id=""
+										placeholder="Name *"
+										className={css.input_style}
+									/>
+								</div>
+								<div className={css.input_wrapper}>
+									{/* <label htmlFor="" className="">Name*</label> */}
+									<input
+										type="text"
+										name=""
+										id=""
+										placeholder="Your Email *"
+										className={css.input_style}
+									/>
+								</div>
+								<div className={css.input_wrapper}>
+									{/* <label htmlFor="" className="">Name*</label> */}
+									<input
+										type="text"
+										name=""
+										id=""
+										placeholder="Phone Number *"
+										className={css.input_style}
+									/>
+								</div>
+							</div>
+							<div className="flex flex-col items-center text-center md:items-center font-quicksand text-theme_dark_blue text-[14px] md:text-[16px]">
+								<p>
+									Your email is only used for order confirmation/notifications
+									and your phone
+								</p>
+								<p>number only for delivery queries.</p>
+							</div>
+							<h1 className={css.form_heading}>DISCOUNT VOUCHER</h1>
+
+							<div className={css.input_wrapper}>
+								{/* <label htmlFor="" className="">Name*</label> */}
+								<input
+									type="text"
+									name=""
+									id=""
+									placeholder="Enter Code"
+									className={css.input_style}
+								/>
+							</div>
+							<h1 className={css.form_heading}>PAYMENT</h1>
+							<div className="flex items-center flex-col md:flex-row w-full gap-y-6 gap-x-4 border-0 md:border-b  ">
+								<div className="flex-1 flex  items-center border-b w-full md:border-none">
+									<div className="relative h-[15px] w-[20px] mr-2">
+										<Image
+											alt="card"
+											src={"/assets/png/card.png"}
+											layout="fill"
+											objectFit="contain"
+										/>
+									</div>
+									{/* <label htmlFor="" className="">Name*</label> */}
+									<input
+										type="text"
+										name=""
+										id=""
+										placeholder="Card Number"
+										className={`${css.input_style_card} flex-1`}
+									/>
+								</div>
+
+								{/* <label htmlFor="" className="">Name*</label> */}
+								<div className="w-full md:w-auto border-b md:border-none">
+									<input
+										type="text"
+										name=""
+										id=""
+										placeholder="MM / YY  CVC"
+										className={css.input_style_card}
+									/>
+								</div>
+							</div>
+						</div>
+					</div>
+
+					<div className="px-4 sm:px-8 py-8 md:px-[150px] md:py-[100px] text-center bg-[#FAFCF8]">
+						<h1 className={`${css.form_heading} text-center `}>SUMMARY</h1>
+
+						<div className="flex flex-col items-center gap-3">
+							<h2 className={`${css.head_common} text-[#38423B] mt-[30px]`}>
+								Your download will be a digital print.
+							</h2>
+							<a
+								className={`${css.head_common}  font-medium  text-[#4C6CAC] underline underline-offset-4`}
+							>
+								Need to go back and check?
+							</a>
+
+							<div className="flex items-center gap-2 mt-11">
+								<label htmlFor="" className="font-quicksand text-[19px] ">Quantity : </label>
+								<input
+									type="number"
+									name=""
+									id=""
+									placeholder="1"
+									className={`${css.input_style_card} flex-1 border-b`}
+								/>
+							</div>
+
+
+						</div>
+						<div className="flex flex-col items-center gap-5">
+							<h1 className="font-quicksand font-medium text-[32px] md:text-[36px] lg:text-[42px] text-[#4C6CAC] text-center mt-12">
+								$ 20.00</h1>
+								<button className="font-quicksand font-bold text-[20px] text-white w-full md:w-[400px] py-3 bg-theme_green ">Order</button>
 						</div>
 
 					</div>
-
-
 				</div>
-
-
 			</div>
 
 			{/* Footer */}
 			<Footer />
 			{/*Footer End  */}
-
 		</div>
-
 	);
-};;
+};
 
 export default CheckOut;
-
