5abb29381668c7d16012ade4f2fa3594cc4bec08
Hannan Hafeez <30556614+hannanhafeez@users.noreply.github.com>
2022-09-28 18:50:43 +0500

    Images updated!
    Signed-off-by: Hannan Hafeez <30556614+hannanhafeez@users.noreply.github.com>

--- next.config.js
+++ next.config.js
@@ -10,7 +10,7 @@ const nextConfig = {
   env: {
     nextImageExportOptimizer_imageFolderPath: "public/images",
     nextImageExportOptimizer_exportFolderPath: "out",
-    nextImageExportOptimizer_quality: 75,
+    nextImageExportOptimizer_quality: 90,
     nextImageExportOptimizer_storePicturesInWEBP: true,
     // nextImageExportOptimizer_generateAndUseBlurImages: true,
   },
